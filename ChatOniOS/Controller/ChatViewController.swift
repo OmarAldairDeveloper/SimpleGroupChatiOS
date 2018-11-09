cb993dd4d62eaedbe8b66fca77bd7d4f0a45a0fa
Omar Aldair <omaraldair.developer@gmail.com>
2018-11-09 14:00:45 -0600

    Primer commit

---
+++ ChatOniOS/Controller/ChatViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ChatViewController.swift
+//  ChatOniOS
+//
+//  Created by Omar Aldair Romero Pérez on 11/9/18.
+//  Copyright © 2018 Omar Aldair Romero Pérez. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ChatOniOS/Controller/WelcomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatOniOS
+//
+//  Created by Omar Aldair Romero Pérez on 11/9/18.
+//  Copyright © 2018 Omar Aldair Romero Pérez. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
