50f0b861268ed07022e4217a41a55e6840f446e1
Omar Aldair <omaraldair.developer@gmail.com>
2018-11-22 13:23:32 -0600

    Hicimos que el user entre directamente al chat en caso de ya tener iniciada una sesión

--- ChatOniOS/Controller/WelcomeViewController.swift
+++ ChatOniOS/Controller/WelcomeViewController.swift
@@ -7,13 +7,30 @@
 //
 
 import UIKit
+import Firebase
 
 class WelcomeViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var entryButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var createAccountButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if Auth.auth().currentUser != nil{
+            performSegue(withIdentifier: "welcomeToChat", sender: self)
+        }
     }
+    
+    
+    @IBAction func entryAction(_ sender: UIButton) {
+        performSegue(withIdentifier: "welcomeToChat", sender: self)
+    }
+    
 
 
 }
